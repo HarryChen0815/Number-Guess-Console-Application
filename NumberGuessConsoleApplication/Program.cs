0650e8e76ed86f8e67f11a2d3eeab1cb037c838a
Harry Chen <qiyu.chen0208@gmail.com>
2024-04-17 12:56:48 +1200

    less the hardcode

--- NumberGuessConsoleApplication/Program.cs
+++ NumberGuessConsoleApplication/Program.cs
@@ -7,56 +7,78 @@ namespace NumberGuessConsoleApplication
         static void Main(string[] args)
         {
             Random random = new Random();
-            int aim = random.Next(1, 100);
-            int times = 0; 
+            int maxRange = 100;
+            int minRange = 1;
+            int aim = random.Next(minRange, maxRange);
+            int times = 0;
             int guess;
             string input;
             string instruction;
 
-            Console.WriteLine("Welcome to the Number Guess Console Application");
-            Console.WriteLine("There is an expected value between 1 and 100. Please Guess it");
-            Console.WriteLine(aim);
+            const string welcomeMessage = "Welcome to the Number Guess Console Application";
+            string instructionMessage = $"There is an expected value between {minRange} and {maxRange}. Please guess it";
+            string newInstructionMessage = $"The new aim value between {minRange} and {maxRange} was generated. Please guess it";
+            string invalidNumberMessage = $"Please enter a valid value between {minRange} and {maxRange}.";
+            const string remindInputMessage = "Please enter your guess value";
+            const string invalidFormatMessage = "Please enter a valid format input.";
+            const string higherHint = "You value is higher than the expected, please try again.";
+            const string lowerHint = "You value is lower than the expected, please try again.";
+            const string congratulations = "Congratulations! your value is the expected";
+            const string playAgainMessage = "Do you want to try it again? (Y/N)";
+            const string goodbyeMessage = "End the game. Goodbye!";
 
-            while (true) {
-                Console.WriteLine("Please enter your guess value");
+            Console.WriteLine(welcomeMessage);
+            Console.WriteLine(instructionMessage);
+            // Console.WriteLine(aim);
+
+            while (true)
+            {
+                Console.WriteLine(remindInputMessage);
                 input = Console.ReadLine();
 
                 if (!int.TryParse(input, out guess))
                 {
-                    Console.WriteLine("Please enter a valid format input.");
+                    Console.WriteLine(invalidFormatMessage);
                     continue;
                 }
 
-                if (guess > 100 || guess < 1){
-                    Console.WriteLine("Please enter a valid value between 1 and 100.");
+                if (guess > maxRange || guess < minRange)
+                {
+                    Console.WriteLine(invalidNumberMessage);
                     continue;
                 }
 
                 times += 1;
-                
-                if (guess > aim){
-                    Console.WriteLine("You value is higher than the expected, please try again.");
+
+                if (guess > aim)
+                {
+                    Console.WriteLine(higherHint);
                 }
-                else if (guess < aim){
-                    Console.WriteLine("You value is lower than the expected, please try again.");
+                else if (guess < aim)
+                {
+                    Console.WriteLine(lowerHint);
                 }
-                else if (guess == aim){
-                    Console.WriteLine("Congratulations! your value is the expected");
-                    Console.WriteLine("You used " + times + " times to find the expected value");
-                    Console.WriteLine("");
-                    Console.WriteLine("Do you want to try it again? (Y/N)");
+                else if (guess == aim)
+                {
+
+                    Console.WriteLine(congratulations);
+                    Console.WriteLine($"You used {times} times to find the expected value");
+                    Console.WriteLine();
+                    Console.WriteLine(playAgainMessage);
 
                     instruction = Console.ReadLine();
-                    if(instruction.ToUpper() == "Y"){
-                        aim = random.Next(1, 100);
+                    if (instruction.ToUpper() == "Y")
+                    {
+                        aim = random.Next(minRange, maxRange);
                         times = 0;
-                        Console.WriteLine("The new aim value was generated");
-                        Console.WriteLine(aim);
+                        Console.WriteLine(newInstructionMessage);
+                        // Console.WriteLine(aim);
                     }
-                    else {
-                        Console.WriteLine("End the game. Goodbye!");
+                    else
+                    {
+                        Console.WriteLine(goodbyeMessage);
                         break;
-                    }      
+                    }
                 }
             }
         }
